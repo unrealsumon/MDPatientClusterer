15114edd263a28218426716680b18f7d9da6c550
unrealsumon <sumon_neo@live.com>
2019-04-07 21:43:25 +1000

    CalculateController Completed.

--- MDPatientClusterer/Controllers/Patient-Groups/CalculateController.cs
+++ MDPatientClusterer/Controllers/Patient-Groups/CalculateController.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MDPatientClusterer.Business;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace MDPatientClusterer.Controllers.Patient_Groups
 {
-    [Route("api/[controller]")]
+    [Route("api/Patient-Groups/[controller]")]
     [ApiController]
     public class CalculateController : ControllerBase
     {
-    }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult Post([FromBody] JObject jObject)
+        {
+            ClusterManager manager = new ClusterManager();                  //declaring the patient manager class.
+            int patientGroups = manager.GetClusters(jObject);               //passing the json object to getclusters function to find clusters.
+            return Ok(new { numberOfGroups = patientGroups });              //returning the result.
+        
+        }
+    }   
+    
 }
