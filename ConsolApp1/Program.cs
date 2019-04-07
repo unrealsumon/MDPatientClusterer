6cce3aa08093108628479b53620578aaf7894164
unrealsumon <sumon_neo@live.com>
2019-04-07 22:04:14 +1000

    Create and populate matrix with random numbers.

--- ConsolApp1/Program.cs
+++ ConsolApp1/Program.cs
@@ -6,7 +6,39 @@ namespace ConsolApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] matrix = new int[5, 5];
+            Random random = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = random.Next(0, 2);
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (matrix[i, j] == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(matrix[i, j] + "  ");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.Write(matrix[i, j] + "  ");
+                    }
+                }
+
+                Console.WriteLine(Environment.NewLine);
+
+
+            }
+
+            Console.ReadKey();
         }
     }
 }
