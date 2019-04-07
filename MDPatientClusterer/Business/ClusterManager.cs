6d17d4fa2e0ae6a98b539e476a63a19b8dad0018
unrealsumon <sumon_neo@live.com>
2019-04-08 03:20:43 +1000

    API | Copy CheckRightBottomCorner function from ConsolApp to ClusterManager

--- MDPatientClusterer/Business/ClusterManager.cs
+++ MDPatientClusterer/Business/ClusterManager.cs
@@ -152,6 +152,44 @@ namespace MDPatientClusterer.Business
         }
 
 
+        //To check all the cells which are at the right bottom corner of current cell
+        private static void CheckRightBottomCorner(int row, int col, int[,] PatientMatrix, bool[,] IsVisited, ref Dictionary<string, Node> NodeToVisit)
+        {
+
+            int colLen = PatientMatrix.GetLength(1);                                // getting the columns length        
+            int rowLen = PatientMatrix.GetLength(0);                                // getting the row length    
+
+            for (int c = col + 1; c < colLen; c++)                                  // start moving right from the current cell
+            {
+                if (row < rowLen - 1)                                               // stopping just before the last row
+                {
+                    row++;                                                          // moving to next row
+                    if (PatientMatrix[row, c] == 1 && !IsVisited[row, c])           // if unvisited node found with value 1
+                    {
+                        Node aNode = new Node();
+                        aNode.r = row;
+                        aNode.c = c;
+                        aNode.key = aNode.r.ToString() + aNode.c.ToString();         //creating the dictionary key using the index  
+
+                        if (NodeToVisit.ContainsKey(aNode.key) == false)             //if not found in NodetoVisit Dictionary
+                        {
+                            aNode.IsVisited = false;
+                            NodeToVisit.Add(aNode.key, aNode);                       //adding to dictionary to visit later.
+                        }
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+
 
         public class Node
         {
